1273fbb3b26e4cd876bd2604df59a98694767b67
Raj2902Patel <rajpatel.imscit20@gmail.com>
2024-09-29 21:00:55 +0530

    Final Changes!

--- lib/pages/razor_pay.dart
+++ lib/pages/razor_pay.dart
@@ -1,8 +1,9 @@
-import 'dart:async';
-
+import 'dart:convert';
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:http/http.dart' as http;
+import 'razor_credentials.dart' as razorCredentials;
 import 'package:lottie/lottie.dart';
 import 'package:random_string/random_string.dart';
 import 'package:razorpay_flutter/razorpay_flutter.dart';
@@ -17,50 +18,139 @@ class RazorPay extends StatefulWidget {
 
 class _RazorPayState extends State<RazorPay> {
   final TextEditingController _amountController = TextEditingController();
-  late Razorpay _razorPay;
-
-  void _handlePaymentSuccess(PaymentSuccessResponse response) {
-    print(
-        "Success -> ${response.orderId} -> ${response.paymentId} -> ${response.signature}");
-    Navigator.pushReplacement(
-        context, MaterialPageRoute(builder: (context) => const SuccessPage()));
-  }
-
-  void _handlePaymentError(PaymentFailureResponse response) {}
-
-  void _handleExternalWallet(ExternalWalletResponse response) {}
+  final _razorpay = Razorpay();
 
   @override
   void initState() {
-    _razorPay = Razorpay();
-    _razorPay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
-    _razorPay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
-    _razorPay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      _razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, _handlePaymentSuccess);
+      _razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, _handlePaymentError);
+      _razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, _handleExternalWallet);
+    });
     super.initState();
+  }
+
+  void _handlePaymentSuccess(PaymentSuccessResponse response) {
+    print(response);
+    verifySignature(
+      signature: response.signature,
+      paymentId: response.paymentId,
+      orderId: response.orderId,
+    );
+
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(
+        builder: (context) => const SuccessPage(),
+      ),
+    );
+  }
+
+  void _handlePaymentError(PaymentFailureResponse response) {
+    print(response);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(response.message ?? ''),
+      ),
+    );
+  }
+
+  void _handleExternalWallet(ExternalWalletResponse response) {
+    print(response);
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(response.walletName ?? ''),
+      ),
+    );
+  }
+
+// create order
+  void createOrder() async {
+    String username = razorCredentials.keyId;
+    String password = razorCredentials.keySecret;
+    String basicAuth =
+        'Basic ${base64Encode(utf8.encode('$username:$password'))}';
+
+    Map<String, dynamic> body = {
+      "amount": double.parse(_amountController.text) * 100,
+      "currency": "INR",
+      "receipt": "recp_${randomAlphaNumeric(10)}",
+    };
+    var res = await http.post(
+      Uri.https(
+          "api.razorpay.com", "v1/orders"), //https://api.razorpay.com/v1/orders
+      headers: <String, String>{
+        "Content-Type": "application/json",
+        'authorization': basicAuth,
+      },
+      body: jsonEncode(body),
+    );
+
+    if (res.statusCode == 200) {
+      openGateway(jsonDecode(res.body)['id']);
+    }
+    print(res.body);
+  }
+
+  openGateway(String orderId) {
+    var options = {
+      'key': razorCredentials.keyId,
+      'amount': double.parse(_amountController.text) *
+          100, //in the smallest currency sub-unit.
+      'name': 'Coffee Shop',
+      'order_id': orderId, // Generate order_id using Orders API
+      'description': 'RazorPay Integration',
+      'timeout': 60 * 5, // in seconds // 5 minutes
+      'prefill': {
+        'contact': '919876543210',
+        'email': 'user@example.com',
+      }
+    };
+    _razorpay.open(options);
+  }
+
+  verifySignature({
+    String? signature,
+    String? paymentId,
+    String? orderId,
+  }) async {
+    Map<String, dynamic> body = {
+      'razorpay_signature': signature,
+      'razorpay_payment_id': paymentId,
+      'razorpay_order_id': orderId,
+    };
+
+    var parts = [];
+    body.forEach((key, value) {
+      parts.add('${Uri.encodeQueryComponent(key)}='
+          '${Uri.encodeQueryComponent(value)}');
+    });
+    var formData = parts.join('&');
+    var res = await http.post(
+      Uri.https(
+        "10.0.2.2", // my ip address , localhost
+        "razorpay_signature_verify.php",
+      ),
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded", // urlencoded
+      },
+      body: formData,
+    );
+
+    print(res.body);
+    if (res.statusCode == 200) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(res.body),
+        ),
+      );
+    }
   }
 
   @override
   void dispose() {
-    _razorPay.clear();
+    _razorpay.clear(); // Removes all listeners
     super.dispose();
-  }
-
-  void openCheckout() async {
-    var options = {
-      'key': "",
-      'amount': double.parse(_amountController.text) * 100,
-      'name': 'Buy Me Coffee!',
-      'description': 'Flutter Course',
-      'prefill': {
-        'contact': '0000000000',
-        'email': 'user@example.com',
-      }
-    };
-    try {
-      _razorPay.open(options);
-    } catch (err) {
-      print("Error $err");
-    }
   }
 
   @override
@@ -147,7 +237,7 @@ class _RazorPayState extends State<RazorPay> {
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.brown.withOpacity(0.5),
               ),
-              onPressed: openCheckout,
+              onPressed: createOrder,
               child: const Text(
                 "ONLINE PAY",
                 style: TextStyle(
